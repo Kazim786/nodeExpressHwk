38560ae035c0aba25c63bcd89cc00e8498de3a30
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-28 13:20:13 -0600

    number 2

--- expresshwk.js
+++ expresshwk.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
     
 })
 
+app.get('/cats', (req, res) => {
+    res.send('Meow')
+    
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('Hello world')
+    
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+    
+})
+
 app.listen(9000, () => {
     console.log("listening to port 9000");
     
